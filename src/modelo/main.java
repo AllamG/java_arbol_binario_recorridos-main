23ac52dfc3ccff3777502cbc48c5224d586ffb80
AllamG <Aguerrad2@miumg.edu.gt>
2023-03-17 05:05:09 +0100

    Se agrega el Proyecto

---
+++ src/modelo/main.java
@@ -0,0 +1,19 @@
+
+package modelo;
+
+/**
+ *
+ * @author paiz2
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         frm_arbol_b frm = new frm_arbol_b();
+        frm.show();
+    }
+    
+}
